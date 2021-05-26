bb86ceb5db94d6e18edec5a9f4c3664fdfc1cc85
Davsav <davidbyu6@gmail.com>
2021-05-26 13:05:33 -0600

    added ability to console log candidates

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const mysql = require('mysql2')
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '2021$@uofu#byu9D',
+        database: 'election'
+
+    },
+    console.log('Connected to the election database.')
+)
+
+
+db.query(`Select * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
 
